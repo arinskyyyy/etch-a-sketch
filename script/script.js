f9b1919b150659468b72fc88a880dc146b4a08b7
arinskyyyy <arinskyyyy@gmail.com>
2026-09-19 17:58:25 +0530

    modified: script/script.js modified: style/styles.css

--- script/script.js
+++ script/script.js
@@ -6,6 +6,31 @@ function createRandomRgb() {
     let red = Math.floor(Math.random * 256);
     let green = Math.floor(Math.random * 256);
     let blue = Math.floor(Math.random * 256);
-    return `rgb(${red}, ${green} ${blue})`
+    return `rgb(${red}, ${green}, ${blue})`
 }
 
+function createGrids(size) {
+    if (!size) {
+        for (let i = 1; i <= 4; i++) {
+            let createGrid = document.createElement("div");
+            createGrid.classListAdd("smallDivs");
+            createGrid.style.backgroundColor = createRandomRgb();
+            createGrid.style.border = "2px solid silver";
+            sketchBox.appendChild(createGrid);
+        }
+    } else {
+        for (let i = 1; i <= size; i++) {
+            let createGrid = document.createElement("div");
+            createGrid.classListAdd("smallDivs");
+            createGrid.style.backgroundColor = createRandomRgb();
+            createGrid.style.border = "2px solid silver";
+            sketchBox.appendChild(createGrid);
+        }
+    }
+}
+createGrids();
+
+resetButton.addEventListener("click", () => {
+    let size = Number.parseInt(prompt("Enter number between 1-100:"));
+    createGrids(size);
+});
